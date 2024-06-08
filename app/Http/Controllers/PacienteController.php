d10e291b6416083b0acfee41337c15828ed2bed0
Yatzari Pecina <2130039@example.com>
2024-06-08 01:09:50 -0600

    controllador crud pacientes

--- app/Http/Controllers/PacienteController.php
+++ app/Http/Controllers/PacienteController.php
@@ -2,25 +2,64 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StorePacienteRequest;
 use App\Models\Paciente;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\View\View;
 
 class PacienteController extends Controller
 {
-    public function index(): View{
-        return view('pacientes', ['pacientes' => Paciente::latest()]);
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
     }
 
-    public function create(): View{
-        return view('registroPacientes');
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
     }
 
-    public function store(StorePacienteRequest $request): RedirectResponse{
-        Paciente::create($request->validated());
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
 
-        return redirect()->route('pacientes.index')->withSuccess('Nuevo paciente agregado');
-    } 
+    /**
+     * Display the specified resource.
+     */
+    public function show(Paciente $paciente)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Paciente $paciente)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Paciente $paciente)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Paciente $paciente)
+    {
+        //
+    }
 }
